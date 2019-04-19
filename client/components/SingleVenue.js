801ff73ebcdbe65c9b8318c2f21b277863780181
perlows <betzalel@perlow.me>
2019-04-19 14:59:12 -0400

    single venue slice is fully functional

--- client/components/SingleVenue.js
+++ client/components/SingleVenue.js
@@ -1,69 +1,75 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {fetchSingleVenue} from '../store/venue'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import {addToCart} from '../store/cart'
-import { Button, Header, Form, Segment } from 'semantic-ui-react'
-
+import {Button, Header, Form, Segment} from 'semantic-ui-react'
 
 class SingleVenue extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {quantity: 1}
     this.handleChange = this.handleChange.bind(this)
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.props.populateVenue(this.props.match.params.id)
   }
 
-  handleAddToCart(venueId){
-    this.props.addToCart(venueId, 1)
+  handleAddToCart(venueId) {
+    this.props.addToCart(venueId, this.state.quantity)
   }
 
-  handleChange(event){
-    console.log(event.target)
+  handleChange(event) {
     this.setState({quantity: event.target.value})
   }
 
-  render () {
-    const { name, type, price, capacity, address, id } = this.props.venue
+  render() {
+    const {name, type, price, capacity, address, id} = this.props.venue
     return (
       <Segment placeholder>
-        <Header >
-          <font size='6'>{name}</font>
+        <Header>
+          <font size="6">{name}</font>
         </Header>
         <h3>Type: {type}</h3>
         <h4>Price: {price}</h4>
         <h4>Capacity: {capacity}</h4>
         <p>Address: {address}</p>
-        {/* <Form>
+        <Form>
           <Form.Group widths="equal">
-            <Form.Field> */}
-                <label htmlFor="quantity">
-                  <small>Quantity</small>
-                </label>
-                <input onChange={this.handleChange} value={this.state.quantity} name="quantity" type="number" />
-            {/* </Form.Field>
+            <Form.Field>
+              <label htmlFor="quantity">
+                <small>Quantity</small>
+              </label>
+              <input
+                onChange={this.handleChange}
+                value={this.state.quantity}
+                name="quantity"
+                type="number"
+              />
+            </Form.Field>
           </Form.Group>
-        </Form> */}
+        </Form>
         <Button
-                  size='huge' color='blue' type="button" onClick={() => this.handleAddToCart(id, this.state.quantity)}
-                >Add to Cart</Button>
+          size="huge"
+          color="blue"
+          type="button"
+          onClick={() => this.handleAddToCart(id, this.state.quantity)}
+        >
+          Add to Cart
+        </Button>
       </Segment>
-      )
-
+    )
   }
-
 }
 
 const mapStateToProps = state => ({
   venue: state.venues.selectedVenue
 })
 
-const mapDispatchToProps = (dispatch) => ({
-  populateVenue: (id) => dispatch(fetchSingleVenue(id)),
+const mapDispatchToProps = dispatch => ({
+  populateVenue: id => dispatch(fetchSingleVenue(id)),
   addToCart: (venueId, quantity) => dispatch(addToCart(venueId, quantity))
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(SingleVenue);
+export default connect(mapStateToProps, mapDispatchToProps)(SingleVenue)
